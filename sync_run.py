c8149dc0a4cc9e50a4b08c99658b81148e88d05f
Ronith Kandallu <ronithkandallu@gmail.com>
2020-10-28 22:12:28 -0700

    Added delay in transmission loop, modified packet chunking procedure

--- sync_run.py
+++ sync_run.py
@@ -3,6 +3,7 @@ from typing import List
 from runup1_pb2 import UploadedRun
 from rundown1_pb2 import DownloadedRun
 import ble_microchip
+import asyncio
 import math
 
 
@@ -12,8 +13,10 @@ async def rx_run() -> DownloadedRun:
         return
 
     packet_count = await ble_microchip.rx_packet_count()
+    print("Packet count: " + str(packet_count))
     packets = []
     for i in range(packet_count):
+        print("Packets received: " + str(i + 1) + "/" + str(packet_count))
         _, received_packet = await ble_microchip.rx_packet()
         packets.append(received_packet)
     return deserialize_run(packets)
@@ -23,31 +26,27 @@ async def tx_run(run: UploadedRun):
     if not ble_microchip.is_connected():
         print("ERROR: Tried writing without active connection")
         return
-        
+
     assert await ble_microchip.rx_client_is_notifiable()
     packets = serialize_run(run)
     packet_count = len(packets)
     await ble_microchip.tx_packet_count(packet_count)
     for i in range(packet_count):
+        await asyncio.sleep(0.05)
         await ble_microchip.tx_packet(packets[i])
 
 
 def serialize_run(run: UploadedRun) -> List[bytes]:
     run_data = run.SerializeToString()
     run_data_size = len(run_data)
-    print(run_data)
-    print(run_data[0].to_bytes(1, "little"))
 
     tx_packet_size = ble_microchip.TX_PACKET_SIZE
     chunked_run_data = []
     for i in range(math.ceil(run_data_size / tx_packet_size)):
-        chunk = bytearray()
-        for j in range(tx_packet_size):
-            index = i * j + j
-            if index >= run_data_size:
-                break
-            chunk.extend(run_data[index].to_bytes(1, "little"))
-        chunked_run_data.append(bytes(chunk))
+        start_index = i * tx_packet_size
+        end_index = start_index + tx_packet_size + 1
+        chunk = run_data[start_index:end_index]
+        chunked_run_data.append(chunk)
     return chunked_run_data
 
 
